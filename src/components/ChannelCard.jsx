3cab9f1550eb4c5ac0da4f0513aa9c779ab3d72a
MZakrawacz <Mikolaj.zakrawacz@gmail.com>
2023-01-18 08:28:22 +0100

    Channel_Card

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Box, CardContent, CardMedia, Topography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from 'react-router-dom';
+import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = () => {
-  return (
-    <div>ChannelCard</div>
-  )
-}
 
+const ChannelCard = ( {ChannelDetail} ) => (
+  <Box sx={{ boxShadow: 'none', borderRadius: '20px'}}>
+    <Link to={`/channel/${ChannelDetail?.id?.channelId}`}>
+      <CardContent sx={{ display: 'flex', flexDirection:'column', justifyContent:'center',textAlign:"center",color:"#fff"}}>
+        <CardMedia image={ChannelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture } alt={ChannelDetail?.snippet?.title} sx={{ borderRadius: '50%', height: '180px', width:'180px', mb: 2, border: '1px solid #e3e3e3'}} />
+      </CardContent>
+    </Link>
+  </Box>
+)
+  
 export default ChannelCard
+
